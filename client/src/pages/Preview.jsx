3c323ad907c77457b2c6322522e1b1e2610e085c
Raj Kalita <rajkalita910@gmail.com>
2025-10-27 13:31:47 +0530

    1stcommit

--- client/src/pages/Preview.jsx
+++ client/src/pages/Preview.jsx
@@ -13,7 +13,7 @@ const [resumeData, setResumeData] = useState(null);
 
 const loadResumeData = async () => {
   try {
-    const {data} = await api.get(`${import.meta.env.VITE_API_BASE_URL}/api/resumes/public/${resumeId}`)
+    const {data} = await api.get(`${import.meta.env.VITE_BASE_URL}/api/resumes/public/${resumeId}`)
 
     setResumeData (data.resume)
   } catch (error) {
